5d5d1cd3f6b0683e6e3b2f37ea991dda9a42b0bd
Christo Lolov <christo_lolov@yahoo.com>
2019-06-26 20:00:57 +0100

    Backup.

--- AwesomeFruit/src/screens/Home.js
+++ AwesomeFruit/src/screens/Home.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRequested: false,
+      spoiledText: "Spoiled!",
+      notSpoiledText: "Not spoiled!",
+      isSpoiled: false
+    };
+
+    this.sendInformation = this.sendInformation.bind(this);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Awesome Fruuit</Text>
+        <Text>Awesome Fruit</Text>
+        <Button
+          onPress={this.sendInformation}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Text>{this.state.isRequested ? (this.state.isSpoiled ? this.state.spoiledText : this.state.notSpoiledText) : ""}</Text>
       </View>
     );
+  }
+
+  sendInformation() {
+    //Do URL call here...
+
+    this.setState(previousState => ({
+      isRequested: true,
+      spoiledText: "Spoiled!",
+      notSpoiledText: "Not spoiled!",
+      isSpoiled: !previousState.isSpoiled
+    }));
   }
 }
 
